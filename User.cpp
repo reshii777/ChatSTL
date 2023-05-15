72ab724b3656cbedfd7eae64ee4108069e97532e
Dmitry <30809886+reshii777@users.noreply.github.com>
2023-05-15 23:46:58 +0300

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,12 +1,17 @@
 #include "User.h"
 
+// Конструктор класса User.
+    // Инициализирует имя и пароль пользователя.
 User::User(const std::string& username, const std::string& password)
-    : username(username), password(password) {}
+    : username(username), password(password) {
+}
 
+// Возвращает имя пользователя.
 std::string User::getName() const {
     return username;
 }
 
+// Возвращает пароль пользователя.
 std::string User::getPassword() const {
     return password;
 }
