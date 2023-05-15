bdf15d21a82671deebc6fc2250e1d26bdc6ed219
Dmitry <30809886+reshii777@users.noreply.github.com>
2023-05-15 23:47:49 +0300

    Update Chat.h

--- Chat.h
+++ Chat.h
@@ -9,18 +9,18 @@
 
 class Chat {
 private:
-    User* currentUser_;
-    std::unordered_map<std::string, User*> users_;
-    std::unordered_map<std::string, std::vector<Message>> messages;
+    User* currentUser_;  // Текущий пользователь
+    std::unordered_map<std::string, User*> users_;  // Коллекция пользователей
+    std::unordered_map<std::string, std::vector<Message>> messages;  // Коллекция сообщений
 
 public:
-    Chat();
-    ~Chat();
+    Chat();  // Конструктор класса Chat
+    ~Chat();  // Деструктор класса Chat
 
-    void registerUser();
-    void sendGroupMessage(const std::string& message);
-    void sendUserMessage(User* sender, const std::string& recipient, const std::string& message);
-    void viewMessages();
-    void viewUsers();
-    void loginUser();
+    void registerUser();  // Регистрация нового пользователя
+    void sendGroupMessage(const std::string& message);  // Отправка группового сообщения
+    void sendUserMessage(User* sender, const std::string& recipient, const std::string& message);  // Отправка сообщения пользователю
+    void viewMessages();  // Просмотр сообщений
+    void viewUsers();  // Просмотр пользователей
+    void loginUser();  // Вход пользователя в систему
 };
