144ca0544d893c50b42e8aa1df46467130dee405
Dmitry <30809886+reshii777@users.noreply.github.com>
2023-05-15 23:47:31 +0300

    Update Message.cpp

--- Message.cpp
+++ Message.cpp
@@ -1,20 +1,27 @@
 #include "Message.h"
 
+// Конструктор класса Message.
+// Инициализирует отправителя и текст сообщения.
 Message::Message(User* sender, const std::string& text)
-    : sender(sender), text(text) {}
+    : sender(sender), text(text) {
+}
 
+// Возвращает указатель на отправителя сообщения.
 User* Message::getSender() const {
     return sender;
 }
 
+// Возвращает текст сообщения.
 std::string Message::getText() const {
-    return text;
+    return text;  
 }
 
+// Устанавливает отправителя сообщения.
 void Message::setSender(User* sender) {
     this->sender = sender;
 }
 
+// Устанавливает текст сообщения.
 void Message::setText(const std::string& text) {
-    this->text = text;
+    this->text = text;  
 }
